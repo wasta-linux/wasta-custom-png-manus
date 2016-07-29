8a1fe89d9e799813b69f6b39cf6b698b70f50fec
pngbill <bill_martin@sil.org>
2016-07-29 16:44:02 -0500

    Add some customizations for Manus including the LibreOffice Extension

--- wasta-custom-png-manus/wasta-custom-png-manus-postinst.sh
+++ wasta-custom-png-manus/wasta-custom-png-manus-postinst.sh
@@ -8,6 +8,9 @@
 #       is only intended to be run at package installation.  
 #
 #   2014-04-25 rik: initial script
+#   2015-06-14 whm: reset the menu hover-delay from 200 back to 0 for better
+#       menu operation on slow Lenovo x1??e laptops. Reinstall gimp (that was
+#       removed in wasta-base-setup, and install gnote.
 #
 # ==============================================================================
 
@@ -76,6 +79,60 @@ mkdir -p /usr/share/wasta-menus/
 ln -sf /usr/share/wasta-custom-png-manus/wasta-menus/wasta-menus-default-apps-png-manus.txt \
     /usr/share/wasta-menus/wasta-menus-default-apps.txt
 
+# Reset the menu hover-delay from 200 back to 0 (works better on slow Lenovo x1??e machines)
+echo
+echo "*** Setting Main Menu Hover Delay (back to 0)"
+echo
+
+sed -i -e 's@\(\"default\" \:\) 200@\1 0@' \
+    /usr/share/cinnamon/applets/menu@cinnamon.org/settings-schema.json
+
+# ------------------------------------------------------------------------------
+# LibreOffice Preferences Extension install (for all users)
+# ------------------------------------------------------------------------------
+
+# REMOVE "Wasta-English-Intl-Defaults" extension: remove / reinstall is only
+#   way to update
+EXT_FOUND=$(ls /var/spool/libreoffice/uno_packages/cache/uno_packages/*/wasta-english-intl-defaults.oxt* 2> /dev/null)
+
+if [ "$EXT_FOUND" ];
+then
+    unopkg remove --shared wasta-english-intl-defaults.oxt
+fi
+
+# Install wasta-english-intl-defaults.oxt (Default LibreOffice Preferences)
+echo
+echo "*** Installing/Updating Wasta English Intl Default LO Extension"
+echo
+unopkg add --shared /usr/share/wasta-custom-png-manus/resources/wasta-english-intl-defaults.oxt
+
+
+# IF user has not initialized LibreOffice, then when adding the above shared
+#   extension, the user's LO settings are created, but owned by root so
+#   they can't change them: solution is to just remove them (will get recreated
+#   when user starts LO the first time).
+
+for LO_FOLDER in /home/*/.config/libreoffice;
+do
+    LO_OWNER=$(stat -c '%U' $LO_FOLDER)
+
+    if [ "$LO_OWNER" == "root" ];
+    then
+        echo
+        echo "*** LibreOffice settings owned by root: resetting"
+        echo "*** Folder: $LO_FOLDER"
+        echo
+    
+        rm -rf $LO_FOLDER
+    fi
+done
+
+# ------------------------------------------------------------------------------
+# Set system-wide Paper Size
+# ------------------------------------------------------------------------------
+# Note: This sets /etc/papersize.  However, many apps do not look at this
+#   location, but instead maintain their own settings for paper size :-(
+paperconfig -p a4
 # ------------------------------------------------------------------------------
 # Finished
 # ------------------------------------------------------------------------------
